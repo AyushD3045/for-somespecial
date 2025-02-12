564cc059933a919857b164af8de8a985b006d4e5
AyushD3045 <ayushdongre1114@gmail.com>
2025-02-12 10:45:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("hugBtn").addEventListener("click", function() {
+    document.getElementById("hugMessage").classList.remove("hidden");
+    document.getElementById("hugImage").classList.remove("hidden");
+});
